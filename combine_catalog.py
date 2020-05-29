5196b2ef82b41ac8768d23478f41cfe7dfc20ebf
MatheusJCastro <matheusdejesuscastro@gmail.com>
2020-05-29 16:20:38 -0300

    New version: bug fixes, now combined catalogs can be saved as fits catalogs (.cat)

--- combine_catalog.py
+++ combine_catalog.py
@@ -1,12 +1,10 @@
 ###################################################
 # Combine Fits Catalog                            #
 # Matheus J. Castro                               #
-# Version 2.1                                     #
-# Last Modification: 01/14/2020 (month/day/year)  #
+# Version 3.0                                     #
+# Last Modification: 05/29/2020 (month/day/year)  #
 ###################################################
 
-import numpy as np
-from astropy.table import Table
 import manage_catalog as mancat
 import time
 
@@ -39,9 +37,14 @@ def main(cat_name_1, cat_name_2, first_combine=0, threshold=3, mag_to_use1="MAG_
     final_cat = mancat.combine_cat(elements, data, objects)
 
     if save_cross_match:
+        cat_to_save = []
+        for i in final_cat:
+            cat_to_save.append(i.tolist())
+        final_cat = cat_to_save
+
         hd = "Number, X_IMAGE, Y_IMAGE, MAG_AUTO, MAGGER_AUTO, FLAGS, FLAGS_WEIGHT, FWHM_IMAGE, " \
              "BACKGROUND, ALPHA_J2000, DELTA_J2000"
-        mancat.save_cross_match_csv(final_cat, head=hd)
+        mancat.save_cross_match_cat(final_cat, head=hd)
 
     fim = time.time()
     print("Time Python: ", fim - inicio)
@@ -54,14 +57,4 @@ cat_1 = 'j02_rSDSS_2.1s_corrected.cat'
 cat_2 = 'j02_rSDSS_0.21s_corrected.cat'
 cat_combined = main(cat_base, cat_1, extent2=1, mag_to_use2="MAG_AUTO_CORRECTED")
 cat_combined = main(cat_base, cat_2, first_combine=cat_combined, save_cross_match=True, extent2=1,
-                   # mag_to_use2="MAG_AUTO_CORRECTED")
-#a = np.array(cat_combined[0][3])
-#print(a)  #.write("j02_rSDSS_teste.csv", format="csv")
-#print(a.shape)
-#print(a.ndim)
-
-
-# Apagar objetos com flag > que 4?
-# Se a flag for igual, verifica a magerr e fica a com menor erro?
-# Mostrar grafico topcat_x_python
-# Cade o plato dos graficos pro threshold?
+                    mag_to_use2="MAG_AUTO_CORRECTED")
--- manage_catalog.py
+++ manage_catalog.py
@@ -1,8 +1,8 @@
 ###################################################
 # Manage Fits Catalog                             #
 # Matheus J. Castro                               #
-# Version 8.3                                     #
-# Last Modification: 01/12/2020 (month/day/year)  #
+# Version 9.0                                     #
+# Last Modification: 05/29/2020 (month/day/year)  #
 ###################################################
 
 import numpy as np
@@ -229,6 +229,21 @@ def read_cross_match_csv(name_csv):
 ######################################################################################
 
 
+def save_cross_match_cat(cat, head=""):
+    from astropy.table import Table
+    import os
+
+    file_name = "Results_Combined.cat"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    cat = Table(np.asarray(cat), names=head.split(","))
+    cat.write(file_name, format="fits")  # Only create the file if the file_name doesn't exists.
+
+
+######################################################################################
+
+
 def save_cross_match_csv(list_of_mag, ar="", dc="", head=""):
     # Save a csv file that contain the cross-matched objects, their positional in sky
     # and the two mags from both catalogs.
@@ -389,7 +404,7 @@ def combine_cat(headers, cats, matchs):
 
     for i in range(len(cat)):
         if i not in np.array(matchs).T[1] and cat[i][flags2] <= 4:
-            final_cat.append(cat_base[i])
+            final_cat.append(cat[i])
 
     print("len: ", len(final_cat))
 
